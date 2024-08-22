73a874f38c23350283a30e7205684f5f3946b173
juanch013 <felipeparada99@hotmail.com>
2024-08-21 22:41:29 -0300

    add contactController and its interface , with createContact implemented

---
+++ src/contact/controller/contactController.ts
@@ -0,0 +1,21 @@
+import { InternalError } from "../../../common/errors/errors";
+import IContactController from "../interfaces/contactController";
+import { IContactService } from "../interfaces/contactService";
+import {Request,Response} from 'express';
+
+export default class contactController implements IContactController{
+    constactService:IContactService
+    constructor(constactService:IContactService){
+        this.constactService = constactService;
+    }
+
+    async createContact(req:Request,res:Response): Promise<Response> {
+        try {
+            const createContactResponse = await this.constactService.createContact(req.body);
+            return res.status(createContactResponse.code).json(createContactResponse);
+        } catch (error) {
+            console.log(error,"context: createContact")
+            return res.status(InternalError.code).json(InternalError)
+        }
+    }
+}
