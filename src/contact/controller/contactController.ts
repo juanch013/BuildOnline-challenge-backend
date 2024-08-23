beff6cb95b865a9c5a31efca0d088e16380907e6
juanch013 <felipeparada99@hotmail.com>
2024-08-22 23:00:54 -0300

    add updateContact to controller and its interface

--- src/contact/controller/contactController.ts
+++ src/contact/controller/contactController.ts
@@ -1,5 +1,7 @@
 import { InternalError } from "../../../common/errors/errors";
+import { UpdateContactDto } from "../dtos/updateContactDto";
 import IContactController from "../interfaces/contactController";
+import { ContactData } from "../interfaces/contactData";
 import { IContactService } from "../interfaces/contactService";
 import {Request,Response} from 'express';
 
@@ -26,6 +28,24 @@ export default class contactController implements IContactController{
             return res.status(getContactResponse.code).json(getContactResponse);
         } catch (error) {
             console.log(error,"context: getContact")
+            return res.status(InternalError.code).json(InternalError)
+        }
+    }
+
+    async updateContact(req:Request,res:Response): Promise<Response>{
+        try {
+            const {contactId} = req.params
+            const body:UpdateContactDto = req.body
+
+            const contactData:ContactData = {
+                ...body,
+                id:contactId
+            }
+
+            const updateResponse = await this.constactService.updateContact(contactData);
+            return res.status(updateResponse.code).json(updateResponse);
+        } catch (error) {
+            console.log(error,"context: updateContact")
             return res.status(InternalError.code).json(InternalError)
         }
     }
--- src/contact/interfaces/contactController.ts
+++ src/contact/interfaces/contactController.ts
@@ -3,4 +3,5 @@ import { Request,Response } from "express";
 export default interface IContactController{
     createContact(req:Request,res:Response):Promise<Response>;
     getContact(req:Request,res:Response): Promise<Response>;
+    updateContact(req:Request,res:Response): Promise<Response>;
 }
