929afde84e0d6d8be26e500b9e7829c847fc74eb
juanch013 <felipeparada99@hotmail.com>
2024-08-22 23:01:58 -0300

    create updateContactDto and add updateContact endpoint in router

--- src/contact/router/contactRouter.ts
+++ src/contact/router/contactRouter.ts
@@ -4,7 +4,9 @@ import { CreateContactDto } from "../dtos/createContactDto";
 import createContactFactory from "../controller/factories/createContactFactory";
 import validateUuidParam from '../../../middlewares/validateUuidParam/validateUuidParam'
 import getContactFactory from "../controller/factories/getContactFactory";
+import updateContactFactory from "../controller/factories/updateContactFactory";
 import verifyToken from "../../../middlewares/authorization/authorization"
+import { UpdateContactDto } from "../dtos/updateContactDto";
 
 const contactRouter = Router();
 
@@ -12,5 +14,6 @@ contactRouter.use(verifyToken)
 
 contactRouter.post('/',validateDto(CreateContactDto),createContactFactory);
 contactRouter.get('/:contactId',validateUuidParam("contactId"),getContactFactory);
+contactRouter.put('/:contactId',validateUuidParam("contactId"),validateDto(UpdateContactDto),updateContactFactory);
 
 export default contactRouter;
