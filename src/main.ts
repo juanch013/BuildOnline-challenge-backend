57a172c1912b88daec6f5a525e6c328b1bc8d18a
juanch013 <felipeparada99@hotmail.com>
2024-09-09 23:56:50 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,21 @@ import dotenv from 'dotenv'
 import dataSource from "./connection/connection"
 import 'reflect-metadata'
 import path from 'path'
+import cors from 'cors'
 
 dotenv.config()
 
 const app = express();
+const front_host = process.env.FRONT_HOST
+
+app.options('*',cors())
+
+app.use(cors({
+    origin: front_host,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true,
+  }));
 
 app.use('/images', express.static(path.join(__dirname, '../images')));
 app.use(express.json()); 
